8cbc24d8ee0597a1e492b3510d58921471ce1cf1
DC Morales <dc.morales37@gmail.com>
2025-01-03 18:15:10 -0800

    add default boards to local storage

--- src/components/BoardSet.tsx
+++ src/components/BoardSet.tsx
@@ -1,126 +1,166 @@
-import { useContext, useEffect, useState } from "react";
-import { HandlerContext } from "../contexts/HandlerContext";
-import { SavedBoard, UnitModel } from "../models/UnitModel";
-import LevelMeter from "./LevelMeter/LevelMeter";
-import DefaultBoards from "../assets/defaultBoards.json";
+import { useContext, useEffect, useState } from 'react';
+import { HandlerContext } from '../contexts/HandlerContext';
+import { SavedBoard, UnitModel } from '../models/UnitModel';
+import LevelMeter from './LevelMeter/LevelMeter';
+import DefaultBoards from '../assets/defaultBoards.json';
 
 export default function BoardSet() {
+	const [statusMsg, setStatusMsg] = useState('');
+	const { unitHandler, boardStorage } = useContext(HandlerContext);
+	const [leftLevel, setLeftLevel] = useState(
+		unitHandler.updatedModel.inputLeft
+	);
+	const [rightLevel, setRightLevel] = useState(
+		unitHandler.updatedModel.inputRight
+	);
+	const [outLeftLevel, setOutLeftLevel] = useState(
+		unitHandler.updatedModel.outputLeft
+	);
+	const [outRightLevel, setOutRightLevel] = useState(
+		unitHandler.updatedModel.outputRight
+	);
+	const [boards, setBoards] = useState([]);
+	const sliderOrientation = 'vertical';
+	//     const board = unitHandler.updatedModel.boardInfo.loadedBoards[0];
+	//     const pedalOptions = unitHandler.updatedModel.boardInfo.pedalOptions;
+	//     const [editMode, setEditMode] = useState<boolean>(false);
+	//     const [boardName, setBoardName] = useState<string>("New Board");
 
-    const [statusMsg, setStatusMsg] = useState("");
-    const { unitHandler, boardStorage } = useContext(HandlerContext);
-    const [leftLevel, setLeftLevel] = useState(unitHandler.updatedModel.inputLeft);
-    const [rightLevel, setRightLevel] = useState(unitHandler.updatedModel.inputRight);
-    const [outLeftLevel, setOutLeftLevel] = useState(unitHandler.updatedModel.outputLeft);
-    const [outRightLevel, setOutRightLevel] = useState(unitHandler.updatedModel.outputRight);
-    const sliderOrientation = "vertical"
-//     const board = unitHandler.updatedModel.boardInfo.loadedBoards[0];
-//     const pedalOptions = unitHandler.updatedModel.boardInfo.pedalOptions;
-//     const [editMode, setEditMode] = useState<boolean>(false);
-//     const [boardName, setBoardName] = useState<string>("New Board");
+	useEffect(() => {
+		// Check if boards are already in localStorage
+		const storedBoards = localStorage.getItem('boards');
 
+		if (storedBoards) {
+			// If found, use it to initialize the state
+			setBoards(JSON.parse(storedBoards));
+		} else {
+			// If not, use DefaultBoards and store in localStorage
+			setBoards(DefaultBoards);
+			localStorage.setItem('boards', JSON.stringify(DefaultBoards));
+		}
+	}, []);
 
- 
-    useEffect(() => {
-      unitHandler.subscribe("levels", "fx-board-app",  distributeLevels);
-      return () => {
-        unitHandler.unsubscribe("levels", "fx-board-app");
-      };
-  
-    }, []);
+	useEffect(() => {
+		// whenever boards change, store them in localStorage
+		if (boards.length > 0) {
+			localStorage.setItem('boards', JSON.stringify(boards));
+		}
+	}, [boards]);
 
-    function callback(msg: any) {
-        unitHandler.processMessage(msg);
-    }
+	useEffect(() => {
+		unitHandler.subscribe('levels', 'fx-board-app', distributeLevels);
+		return () => {
+			unitHandler.unsubscribe('levels', 'fx-board-app');
+		};
+	}, []);
 
-    async function distributeLevels(model: UnitModel) {
-        setStatusMsg(`Levels: ${JSON.stringify(model)}`);
-        setLeftLevel(model.inputLeft);
-        setRightLevel(model.inputRight);
-        setOutLeftLevel(model.outputLeft);
-        setOutRightLevel(model.outputRight);
-    }
-    
-    async function start_audio() {
-        await unitHandler.startAudio(callback);
-    }
+	function callback(msg: any) {
+		unitHandler.processMessage(msg);
+	}
 
-    async function load_board() {
-        await unitHandler.refreshPedalConfig();
-    }
-    
-    async function stop_audio() {
-        await unitHandler.stopAudio();
-    }
+	async function distributeLevels(model: UnitModel) {
+		setStatusMsg(`Levels: ${JSON.stringify(model)}`);
+		setLeftLevel(model.inputLeft);
+		setRightLevel(model.inputRight);
+		setOutLeftLevel(model.outputLeft);
+		setOutRightLevel(model.outputRight);
+	}
 
-    async function retrieve_boards() {
-        await unitHandler.refreshPedalConfig();
-    }
+	async function start_audio() {
+		await unitHandler.startAudio(callback);
+	}
 
-      // This will fetch a board from the cloud and push it to the unit (or erase the board)
-//   async function loadBoard(_boardId: number) {
-//     setEditMode(false);
-//   }
+	async function load_board() {
+		await unitHandler.refreshPedalConfig();
+	}
 
-    async function saveDefaultBoards() {
-        // boardStorage.clearItems();
-        DefaultBoards.map((b) => {
-            var bd: SavedBoard = {
-                boardId: b.id,
-                name: b.name,
-                channel: 0,
-                pedals: b.config,
-            };
-            boardStorage.setItem(bd);
-        })
-        // for (const b in DefaultBoards) {
-        //     console.log(b);
-        //     // boardStorage.setItem(b);
-        // }
-    }
+	async function stop_audio() {
+		await unitHandler.stopAudio();
+	}
 
+	async function retrieve_boards() {
+		await unitHandler.refreshPedalConfig();
+	}
 
-    async function savePedalsForLater() {
-        await boardStorage.setItem(
-            { ...unitHandler.updatedModel.boardInfo.loadedBoards[0], name: "lefty" }
-        );
-        await boardStorage.setItem(
-            { ...unitHandler.updatedModel.boardInfo.loadedBoards[0], name: "righty" }
-        );
-    }
-   
-    return (
-        <div>
-            <h1>FX Board</h1>
-            <p className="row">
-            <button type="button" onClick={start_audio}>Start</button>
-            <button type="button" onClick={stop_audio}>Stop</button>
-            <button type="button" onClick={retrieve_boards}>Retrieve</button>
-            <button type="button" onClick={load_board}>Guitar</button>
-            <button type="button" onClick={savePedalsForLater}>Save</button>
-            <button type="button" onClick={saveDefaultBoards}>Save default Boards</button>
-            </p>
-            <LevelMeter
-                signal={leftLevel}
-                orientation={sliderOrientation}
-                size={"16rem"}
-            />
-            <LevelMeter
-                signal={outLeftLevel}
-                orientation={sliderOrientation}
-                size={"16rem"}
-            />
-            <LevelMeter
-                signal={rightLevel}
-                orientation={sliderOrientation}
-                size={"16rem"}
-            />
-            <LevelMeter
-                signal={outRightLevel}
-                orientation={sliderOrientation}
-                size={"16rem"}
-            />
-            <div>
-                {/* <Pedalboard
+	// This will fetch a board from the cloud and push it to the unit (or erase the board)
+	//   async function loadBoard(_boardId: number) {
+	//     setEditMode(false);
+	//   }
+
+	async function saveDefaultBoards() {
+		// boardStorage.clearItems();
+		DefaultBoards.map((b) => {
+			var bd: SavedBoard = {
+				boardId: b.id,
+				name: b.name,
+				channel: 0,
+				pedals: b.config,
+			};
+			boardStorage.setItem(bd);
+		});
+		// for (const b in DefaultBoards) {
+		//     console.log(b);
+		//     // boardStorage.setItem(b);
+		// }
+	}
+
+	async function savePedalsForLater() {
+		await boardStorage.setItem({
+			...unitHandler.updatedModel.boardInfo.loadedBoards[0],
+			name: 'lefty',
+		});
+		await boardStorage.setItem({
+			...unitHandler.updatedModel.boardInfo.loadedBoards[0],
+			name: 'righty',
+		});
+	}
+
+	return (
+		<div>
+			<h1>FX Board</h1>
+			<p className="row">
+				<button type="button" onClick={start_audio}>
+					Start
+				</button>
+				<button type="button" onClick={stop_audio}>
+					Stop
+				</button>
+				<button type="button" onClick={retrieve_boards}>
+					Retrieve
+				</button>
+				<button type="button" onClick={load_board}>
+					Guitar
+				</button>
+				<button type="button" onClick={savePedalsForLater}>
+					Save
+				</button>
+				<button type="button" onClick={saveDefaultBoards}>
+					Save default Boards
+				</button>
+			</p>
+			<LevelMeter
+				signal={leftLevel}
+				orientation={sliderOrientation}
+				size={'16rem'}
+			/>
+			<LevelMeter
+				signal={outLeftLevel}
+				orientation={sliderOrientation}
+				size={'16rem'}
+			/>
+			<LevelMeter
+				signal={rightLevel}
+				orientation={sliderOrientation}
+				size={'16rem'}
+			/>
+			<LevelMeter
+				signal={outRightLevel}
+				orientation={sliderOrientation}
+				size={'16rem'}
+			/>
+
+			<div>
+				{/* <Pedalboard
                     channel={0}
                     pedals={board.pedals}
                     pedalOptions={pedalOptions}
@@ -134,9 +174,8 @@ export default function BoardSet() {
                     saveFunc={savePedalsForLater}
                     loadBoard={loadBoard}
                 /> */}
-            </div>
-            <p>{statusMsg}</p>
-
-        </div>
-    )
+			</div>
+			<p>{statusMsg}</p>
+		</div>
+	);
 }
